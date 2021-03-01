fce22a4bd92c0e033c0b9cbca0aa951a6ad3b802
Rizzo <ctrizzo33@gmail.com>
2021-03-01 18:49:32 -0500

    trying to use components

---
+++ client3/src/views/LogIn.vue
@@ -0,0 +1,35 @@
+<template>
+    <MyNav>
+
+      <h1 class="title">Existing Users Log In
+    <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="e.g carlorizzo33@gmail.com">
+        </div>
+      </div>
+  </h1>
+
+  <div class="field">
+    <label class="label">Password</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="e.g zzxxcc111">
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import MyNav from "../components/MyNav";
+export default{
+    components: {
+        MyNav
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
