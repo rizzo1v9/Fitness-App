e1ea523f0bf9d409118c0795051be00142ec787b
Rizzo <ctrizzo33@gmail.com>
2021-03-01 15:42:41 -0500

    added server code

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
 /* server
 */
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+    .get('/', (req, res) => {
+    res.send('Hello World!')
+    })
+    .get('/purim', (req, res) => {
+        res.send('Lechaim, to life!');
+    })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
