e7e14d49c7ba4fcdf792e9119f230251fb7e66ae
Rizzo <ctrizzo33@gmail.com>
2021-03-04 14:47:06 -0500

    fixed typo

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 /* server
 */
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
-    .get('/', (req, res) => {
+    use(express.static('./docs'))
+  .get('/', (req, res) => {
     res.send('Hello World!')
     })
     .get('/purim', (req, res) => {
         res.send('Lechaim, to life!');
+    })
+    .get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'docs/index.html'));
     })
 
 app.listen(port, () => {
